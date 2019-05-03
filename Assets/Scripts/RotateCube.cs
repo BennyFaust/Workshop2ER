739321ac26dce3b7167ed5e77e0a1ff51ef7fe41
xSorRx <xSorRx@users.noreply.github.com>
2019-05-03 16:10:55 +0200

    NotFixed

--- Assets/Scripts/RotateCube.cs
+++ Assets/Scripts/RotateCube.cs
@@ -9,7 +9,6 @@ public class RotateCube : MonoBehaviour {
 
     void FixedUpdate()
     {
-		transform.Rotate(Input.GetAxis("Vertical")*speed*Time.deltaTime, Input.GetAxis("Oculus_CrossPlatform_PrimaryIndexTrigger") *-speed*Time.deltaTime, Input.GetAxis("Horizontal")*(-speed)*Time.deltaTime);
-        transform.Rotate(0, Input.GetAxis("Oculus_CrossPlatform_SecondaryIndexTrigger") *speed * Time.deltaTime, 0);
+        transform.Rotate(Input.GetAxis("Vertical") * speed * Time.deltaTime, 0, Input.GetAxis("Horizontal") * (-speed) * Time.deltaTime);
     }
 }
